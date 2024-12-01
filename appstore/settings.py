d08054a1024bdec9baf1ee11ab7c490b97f7ec2c
Nilton taco <nilton2005marzo@gmail.com>
2024-12-01 17:05:59 -0500

    fix default_file_storges

--- appstore/settings.py
+++ appstore/settings.py
@@ -94,8 +94,8 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL', 'https://nyc3.digitaloceanspaces.com')
-DEFAULT_FILE_STORAGE = 'storages.backend.s3boto3.S3Boto3Storage'
 MEDIA_URL = f"{AWS_S3_ENDPOINT_URL}/{AWS_STORAGE_BUCKET_NAME}/"
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
