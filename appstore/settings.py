ed6e2fcb0a9b3cc2849bd55fe3826e17754564a6
Nilton taco <nilton2005marzo@gmail.com>
2024-12-01 18:04:24 -0500

    fix upload file images

--- appstore/settings.py
+++ appstore/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'appstore.wsgi.application'
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL', 'https://techunter.nyc3.digitaloceanspaces.com')
+AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL', 'https://techunterim.nyc3.digitaloceanspaces.com')
 MEDIA_URL = f"{AWS_S3_ENDPOINT_URL}/"
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
